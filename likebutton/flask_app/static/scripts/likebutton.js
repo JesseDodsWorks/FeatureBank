87825c18a1c6aed0d0d9ae183c1e4d713ed7d45e
Jesse Dods <ghostling425@gmail.com>
2022-11-13 02:26:14 -0800

    fix js logic, added basic anim, un anim on unlike

--- likebutton/flask_app/static/scripts/likebutton.js
+++ likebutton/flask_app/static/scripts/likebutton.js
@@ -1,12 +1,31 @@
 
 function likedPost() {
-    if(document.getElementById("lb").class != "liked") {
-        document.getElementById("lb").classList.toggle("unliked");
-        document.getElementById("lb").classList.toggle("liked");
-    
-    }
-    else if(document.getElementById("lb").class != "unliked"){
-        document.getElementById("lb").classList.toggle("liked");
-        document.getElementById("lb").classList.toggle("unliked");
+    document.getElementById("lb").classList.toggle("liked")
+    document.getElementById("lb").classList.toggle("unliked")
+
+    if(document.getElementById("lb").classList.value == "bi bi-hand-thumbs-up liked"){
+        console.log("hello")
+        likeAnimate()
     }
 }
+
+function likeAnimate() {
+    let spans =  document.getElementsByTagName("span");
+    for(span of spans){
+        span.classList.add("anim");
+    }
+    setTimeout(stopAnimate,500);
+}
+
+function stopAnimate() {
+let spans =  document.getElementsByTagName("span");
+for(span of spans){
+    span.classList.remove("anim");
+}
+}
+
+    // let a = document.getElementById("lb");
+    // let spans =  document.getElementsByTagName("span");
+    // for(span of spans){
+    //     span.classList.remove("anim");
+    // }
